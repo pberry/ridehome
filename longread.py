b878ccdbb3b8afd6edc68c7b9396ca9d9f6524e4
Patrick Berry <pberry@gmail.com>
2018-11-26 19:46:30 -0800

    Cleanup, and then making it dirty again

--- longread.py
+++ longread.py
@@ -4,14 +4,20 @@ import re
 import time
 from bs4 import BeautifulSoup
 
+longReads = 0
+longReadWeeks = 0
 rhfeed = feedparser.parse('https://techmeme.com/techmeme-ride-home-feed')
 for post in rhfeed.entries:
-   	if "Longread" in post.summary :
-   		soup = BeautifulSoup(post.summary, 'html.parser')
-   		postPubTime = time.strftime("%A, %B %d" ,post.published_parsed)
+	soup = BeautifulSoup(post.summary, 'html.parser')
+	if "Longread" in post.summary :
+		longReadWeeks += 1
+		postPubTime = time.strftime("%A, %B %d" ,post.published_parsed)
 
-   		for paragraph in soup.find_all('p'):
-   			if "Longreads:" in paragraph.text:
-   				print ("\n**" + postPubTime + "**")
-   				for listItem in (paragraph.next_sibling.find_all('li')):
-   					print ("* [" + listItem.a.string + "](" + listItem.a['href'] + ")" + listItem.contents[1].string)
+		for paragraph in soup.find_all('p'):
+			if "Longreads:" in paragraph.text:
+				print ("\n**" + postPubTime + "**")
+				for listItem in (paragraph.next_sibling.find_all('li')):
+					longReads += 1
+					print ("* [" + listItem.a.string + "](" + listItem.a['href'] + ")" + listItem.contents[1].string)
+print ("Longreads: ",longReads)
+print ("Longread Weeks: ",longReadWeeks)
