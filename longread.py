8bd6f5ecc7e5130d60214c3f1885bbf96cb3c067
Patrick Berry <pberry@gmail.com>
2018-12-29 12:25:05 -0800

    Remove redundant logic

--- longread.py
+++ longread.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
+import configparser
 import feedparser
 import html2text
 import re
 import time
 from bs4 import BeautifulSoup
 
+config = configparser.ConfigParser()
+config.read('config.cfg')
+pbkey = config["Main"]["pinboardkey"]
 
 rhfeed = feedparser.parse('https://techmeme.com/techmeme-ride-home-feed')
+
 for post in rhfeed.entries:
-	cleanPost = post.summary.replace('\n','')
+	cleanPost = post.content[0]['value'].replace('\n','')
 	soup = BeautifulSoup(cleanPost, 'html.parser')
-	if "Longread" in cleanPost :
+	postPubTime = time.strftime("%A, %B %d" ,post.published_parsed)
 
-		postPubTime = time.strftime("%A, %B %d" ,post.published_parsed)
-
-		for paragraph in soup.find_all('p'):
-			if "Longreads:" in paragraph.text or "Suggestions:" in paragraph.text:
-				print ("**" + postPubTime + "**")
-				print (html2text.html2text(str(paragraph.next_sibling)))
+	for paragraph in soup.find_all('p'):
+		if "Longreads:" in paragraph.text or "Suggestions:" in paragraph.text:
+			print ("**" + postPubTime + "**")
+			print (html2text.html2text(str(paragraph.next_sibling)))
